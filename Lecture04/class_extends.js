a62eb5408ec3330efba874138001eff3a83628be
Vsspnc <visitsak.phe@gmail.com>
2026-01-08 13:57:01 +0700

    save

---
+++ Lecture04/class_extends.js
@@ -0,0 +1,23 @@
+class Media {
+    constructor(info) {
+        this.publishDate = info.publishDate;
+        this.name = info.name;
+    }
+}
+
+class Song extends Media {
+    constructor(songData){
+        super(songData);
+        this.artist = songData.artist;
+    }
+}
+
+const mySong = new Song({
+    artist: "Queen",
+    name: "Bohemian Rhapsody",
+    publishDate: "1975-10-31"
+});
+
+console.log(mySong);
+console.log(mySong.name);
+
